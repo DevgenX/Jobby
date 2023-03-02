5d5de046375b9e650a04e4f67347026fcdd9e537
devgenx <sebastian110420@gmail.com>
2023-03-02 11:14:11 -0500

    Added database connection

---
+++ middleware/not-found.js
@@ -0,0 +1,5 @@
+const notFoundMiddleWare = (req, res) => {
+  res.status(404).send("Route doesn't exist");
+};
+
+export default notFoundMiddleWare;
--- server.js
+++ server.js
@@ -1,1 +1,33 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 
+import connectDB from "./db/connect.js";
+// middleware
+import errorHandlerMiddleWare from "./middleware/error-handler.js";
+import notFoundMiddleWare from "./middleware/not-found.js";
+
+app.get("/", (req, res) => {
+  throw new Error("error");
+  res.send("Welcome!");
+});
+
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleWare);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+
+    app.listen(port, () => {
+      console.log(`Server is running at ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
